c8f78f6477ac5cf542e15cafa3d67486d5421cc6
Gadaleta <jg9902@ad.desales.edu>
2018-11-07 14:54:21 -0500

    sync

--- Project Tables.sql
+++ Project Tables.sql
@@ -1,5 +1,6 @@
 --drop
-
+Drop Table  Person 			CASCADE CONSTRAINTS;
+Drop Table  Employee 		CASCADE CONSTRAINTS;
 --create table
 Create Table Person{
 	Person_ID	varchar2(20),
@@ -11,6 +12,32 @@ Create Table Person{
 };
 
 Create Table Employee(
-	Emp_ID		varchar2(30),
+	Emp_ID		varchar2(20),
 	Constraint Employee_pk Primary Key (Employee_ID)
 );
+
+Create Table Nurse_practitioner(
+	NP_ID		varchar2(20),
+	Constraint Nurse_practitioner_pk Primary Key (NP_ID)
+);
+
+Create Table Registered_Nurses(
+	RN_ID		varchar2(20),
+	Constraint Registered_Nurses_pk Primary Key (RN_ID)
+);
+
+Create Table Pharmacist(
+	Pharmacist_ID		varchar2(20),
+	Constraint Pharmacist_pk Primary Key (RN_ID)
+);
+
+Create Table Medical_Technician(
+	MT_ID				varchar2(20),
+	Constraint Medical_Technician_pk Primary Key (MT_ID)
+);
+
+Create Table Physician(
+	Physician_ID				varchar2(20),
+	Constraint Physician_pk Primary Key (Physician_ID)
+);
+
