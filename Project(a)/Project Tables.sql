52d9cff3c9ea0cad6c9eaa13e3d7ffcfab87f345
Jake <jake.gads@gmail.com>
2018-12-03 11:43:52 -0500

    Sync

--- Project(a)/Project Tables.sql
+++ Project(a)/Project Tables.sql
@@ -143,16 +143,20 @@ Create Table Book_Keeper(
 Create Table Bill(
     Bill_ID     varchar2(20),
     Bill_Price  number,
+    
+    Patient_ID		varchar2(20),
+	Constraint Bill_fk 	Foreign Key (Patient_ID) 	REFERENCES Patient(Patient_ID),
+    
+    
 	--*
     Bill_date   date,
     --*
-    Bill_treatment VARCHAR2(25),
+    Bill_treatment VARCHAR2(35),
     --*
     AmountPaid  number,
     --*
     
-    Patient_ID		varchar2(20),
-	Constraint Bill_fk 	Foreign Key (Patient_ID) 	REFERENCES Patient(Patient_ID),
+    
 	
     Constraint Bill_pk Primary Key (Bill_ID)
 );
