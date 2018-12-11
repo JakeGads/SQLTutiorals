4649bf9a1abc00e9c52f8818a4f4b089d7f9dfd8
Jake Gadaleta <jake.gads@gmail.com>
2018-12-11 10:57:12 -0500

    Update Query.sql

--- Project(a)/Query.sql
+++ Project(a)/Query.sql
@@ -10,11 +10,11 @@ Operating_Room_Log:
 Daily_Delivery_Room_Log: 
     This records information about all the deliveries performed each day.
 Recovery Room Log:
-    This report records information about the use of the recovery room, including the patient’s name, attending practitioner, 
+    This report records information about the use of the recovery room, including the patientâ€™s name, attending practitioner, 
     bed, date in, time in, date out, time out, and signature of the practitioner who signs the patient out. A nurse records the 
     times and results of any medical checks performed while the patient is in recovery.
 Monthly_Activity_Report: 
-    This is an internal report summarizing the clinic’s activity each month. It shows such items as the number of visits 
+    This is an internal report summarizing the clinicâ€™s activity each month. It shows such items as the number of visits 
     conducted by each provider, the number of surgeries performed, the number of deliveries, the number of lab tests broken down 
     by type, the number of prescriptions dispensed, the average time per visit, and so on.
     
@@ -127,3 +127,104 @@ SELECT
     nurse_name
 FROM
     operating_room_log;
+    
+    
+    
+    
+    
+    
+    
+    
+---------------------------------------------
+--Lauren
+drop view daily_delivery_room_log;
+drop view recovery_room_log;
+drop view monthly_activity_report;
+
+create view daily_delivery_room_log as 
+select 
+	room.room_type			room_type,
+	visit.start_time		start_time,
+	visit.end_time			end_time,
+	patient.patient_id		patient_id,
+	name.name			patient_name,
+	surgeon2.physician_id		surgeon_id,
+	surgeon.name			surgeon_name,
+	nurse.name			nurse_name,
+	nurse2.registered_nurse_id	nurse_id
+from 
+	room
+	inner join room on room.room_id = room_id
+	inner join visit on room.visit_id = visit.visit_id
+	inner join patient on visit.patient_id = patient.patient_id
+	inner join peron name on name.person_id = patient.person_id
+	inner join physician surgeon2 on surgeon2.physician_id = room.physician_id
+	inner join employee surgeon on surgeon.employee_id = surgeon2.employee_id
+	inner join person surgeon on surgeon2.person_id = surgeon.person_id
+	inner join registered_nurse nurse2 on nurse2.registered_nurse_id = room.registered_nurse_id
+	inner join employee nurse on nurse.employee_id = nurse.employee_id
+	inner join person nurse2 on nurse2.person_id = nurse.person_id
+where
+	room.room_type = 'Birthing Room' and (visit.start_time > TIMESTAMP '2014-14-04 00:00:00' and visit.start_time < TIMESTAMP '2014-14-04 23:59:00' );
+
+
+create view recovery_room_log as 
+select
+	room.room_type			room_type,
+	visit.start_time		start_time,
+	visit.end_time			end_time,
+	patient.patient_id		patient_id,
+	name.name			patient_name,
+	nurse.name			nurse_name,
+	nurse2.registered_nurse_id	nurse_id
+from 
+	room
+	inner join room on room.room_id = room_id
+	inner join visit on room.visit_id = visit.visit_id
+	inner join patient on visit.patient_id = patient.patient_id
+	inner join peron name on name.person_id = patient.person_id
+	inner join registered_nurse nurse2 on nurse2.registered_nurse_id = room.registered_nurse_id
+	inner join employee nurse on nurse.employee_id = nurse.employee_id
+	inner join person nurse2 on nurse2.person_id = nurse.person_id
+where
+	room.room_type = 'Recovery Room' and (visit.start_time > TIMESTAMP '2013-13-03 00:00:00' and visit.start_time < TIMESTAMP '2013-13-03 23:59:00' );
+
+
+create view monthly_activity_report
+select
+	room.room_type			room_type,
+	visit.start_time		start_time,
+	visit.end_time			end_time,
+	patient.patient_id		patient_id,
+	name.name			patient_name,
+	surgeon2.physician_id		surgeon_id,
+	surgeon.name			surgeon_name,
+	nurse.name			nurse_name,
+	nurse2.registered_nurse_id	nurse_id
+from 
+	room
+	inner join room on room.room_id = room_id
+	inner join visit on room.visit_id = visit.visit_id
+	inner join patient on visit.patient_id = patient.patient_id
+	inner join peron name on name.person_id = patient.person_id
+	inner join physician surgeon2 on surgeon2.physician_id = room.physician_id
+	inner join employee surgeon on surgeon.employee_id = surgeon2.employee_id
+	inner join person surgeon on surgeon2.person_id = surgeon.person_id
+	inner join registered_nurse nurse2 on nurse2.registered_nurse_id = room.registered_nurse_id
+	inner join employee nurse on nurse.employee_id = nurse.employee_id
+	inner join person nurse2 on nurse2.person_id = nurse.person_id
+where 
+	room.room_type = 'Pharmacy'
+               OR room_type = 'Lab'
+               OR room_type = 'Birthing Room'
+               OR room_type = 'Operating Room'
+               OR room_type = 'Consultation Room'
+               OR room_type = 'Examination Room'
+               OR room_type = 'Nurses Station'
+               OR room_type = 'Administrative Office'
+               OR room_type = 'Waiting Room'
+               OR room_type = 'Recovery Room' ),
+
+
+
+
